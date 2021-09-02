5af8a5ed235137c67ec84cd632dc4285d0cb3ce2
fernandaMarcillo <86363666+fernandaMarcillo@users.noreply.github.com>
2021-09-02 16:20:10 -0500

    Add files via upload

---
+++ Proyecto-main/script.js
@@ -0,0 +1,24 @@
+function sumar (numero1, numero2){
+    let operacion;
+    operacion = parseFloat(numero1) + parseFloat(numero2);
+    return operacion;
+
+}
+
+function restar (numero1, numero2){
+    let resta;
+    resta = parseFloat(numero1) - parseFloat(numero2);
+    return resta;
+}
+
+function multiplicar(numero1, numero2){
+    let multiplicacion;
+    multiplicacion =parseFloat (numero1) * parseFloat(numero2);
+    return multiplicacion;
+}
+
+function dividir(numero1, numero2){
+    let division;
+    division = parseFloat(numero1) / (numero2);
+    return division;
+}
